480dfed5d821d66c72031fb03b9a44d460487de9
IVYMAX <ivyli@IVYMAXmbp.lan>
2019-05-20 19:03:14 +1000

    card add

--- src/Card.js
+++ src/Card.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import DateDisplay from "./DateDisplay";
 import './App.css';
 
 
+ 
 const Card = function (props){
   
   const {title, img, subTitle} = props
@@ -15,21 +17,24 @@ const Card = function (props){
 
   </div>
 
-
 }
-
+ 
 class App extends Component {
   render() {
     return ( 
       <div className="App">
         <header className="App-header">
         
+        <DateDisplay owner={ {name:'jackie',age:'27'}} />
         <Card title="Dog" subTitle="this is a dog" img="https://picsum.photos/id/237/200/300"/>
-        
+        <Card title="Cat" subTitle="this is a cat" img="https://picsum.photos/id/237/200/300"/>
+        <Card title="bird" subTitle="this is a bird" img="https://picsum.photos/id/237/200/300"/>
+
+
         </header>
       </div>
     );
   }
-}
+} 
 
 export default App;
